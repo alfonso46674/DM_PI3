3aeca05f920fbf404b62be888780b64d034c385c
BuildTools <is713932@iteso.mx>
2021-04-12 17:17:33 -0500

    Feat: Share external news (title, image, description and url)

--- lib/home/noticias_ext_api/item_noticia.dart
+++ lib/home/noticias_ext_api/item_noticia.dart
@@ -1,118 +1,154 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_login/home/formulario/bloc/formulario_bloc.dart';
 import 'package:google_login/models/new.dart';
 import 'package:google_login/models/new_firebase.dart';
+import 'package:share/share.dart';
+import 'package:http/http.dart';
+import 'package:path_provider/path_provider.dart';
 
-class ItemNoticia extends StatelessWidget {
+class ItemNoticia extends StatefulWidget {
   final New noticia;
   ItemNoticia({Key key, @required this.noticia}) : super(key: key);
 
   @override
+  _ItemNoticiaState createState() => _ItemNoticiaState();
+}
+
+class _ItemNoticiaState extends State<ItemNoticia> {
+  @override
   Widget build(BuildContext context) {
     return BlocListener<FormularioBloc, FormularioState>(
       listener: (context, state) {
-        if(state is SavedExteriorNewState){
+        if (state is SavedExteriorNewState) {
           ScaffoldMessenger.of(context)
-          ..hideCurrentSnackBar()
-          ..showSnackBar(SnackBar(
-            content: Text('Noticia guardada en Firebase'),
-          ));
+            ..hideCurrentSnackBar()
+            ..showSnackBar(SnackBar(
+              content: Text('Noticia guardada en Firebase'),
+            ));
         }
       },
 // TODO: Cambiar image.network por Extended Image con place holder en caso de error o mientras descarga la imagen
       child: Container(
-      height: 200,
-      child: Padding(
-        padding: EdgeInsets.all(6.0),
-        child: Card(
-          clipBehavior: Clip.antiAlias,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          child: Row(
-            children: [
-              Expanded(
-                flex: 1,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10.0),
-                  child: Image.network(
-                    "${noticia.urlToImage}",
-                    height: 200,
-                    fit: BoxFit.cover,
+        height: 200,
+        child: Padding(
+          padding: EdgeInsets.all(6.0),
+          child: Card(
+            clipBehavior: Clip.antiAlias,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10.0),
+                    child: Image.network(
+                      "${widget.noticia.urlToImage}",
+                      height: 200,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
-              ),
-              Expanded(
-                flex: 3,
-                child: Padding(
-                  padding: EdgeInsets.all(11.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        "${noticia.title}",
-                        maxLines: 1,
-                        overflow: TextOverflow.clip,
-                        style: TextStyle(
-                          fontWeight: FontWeight.w900,
-                          fontSize: 14,
+                Expanded(
+                  flex: 3,
+                  child: Padding(
+                    padding: EdgeInsets.all(11.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          "${widget.noticia.title}",
+                          maxLines: 1,
+                          overflow: TextOverflow.clip,
+                          style: TextStyle(
+                            fontWeight: FontWeight.w900,
+                            fontSize: 14,
+                          ),
                         ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "${noticia.publishedAt}",
-                            style: TextStyle(
-                              fontWeight: FontWeight.w300,
-                              color: Colors.grey,
-                              fontSize: 12,
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "${widget.noticia.publishedAt}",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w300,
+                                color: Colors.grey,
+                                fontSize: 12,
+                              ),
                             ),
-                          ),
-                          //boton para subir noticia a firebase
-                          IconButton(
-                            icon: Icon(Icons.upload_rounded),
-                            onPressed: () {
-                              BlocProvider.of<FormularioBloc>(context).add(
-                                SaveExteriorElementEvent(
-                                  noticia: NewFirebase(
-                                      author: noticia.author,
-                                      title: noticia.title,
-                                      description: noticia.description,
-                                      urlToImage: noticia.urlToImage),
+                            Row(
+                              children: [
+                                //boton para compartir
+                                IconButton(
+                                  icon: Icon(Icons.share_rounded),
+                                  onPressed: () {
+                                    _saveAndShare(widget.noticia);
+                                  },
                                 ),
-                              );
-                            },
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 16),
-                      Text(
-                        "${noticia.description ?? "Descripcion no disponible"}",
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        "${noticia.author ?? "Autor no disponible"}",
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          fontWeight: FontWeight.w300,
-                          fontSize: 12,
+                                //boton para subir noticia a firebase
+                                IconButton(
+                                  icon: Icon(Icons.upload_rounded),
+                                  onPressed: () {
+                                    BlocProvider.of<FormularioBloc>(context)
+                                        .add(
+                                      SaveExteriorElementEvent(
+                                        noticia: NewFirebase(
+                                            author: widget.noticia.author,
+                                            title: widget.noticia.title,
+                                            description: widget.noticia.description,
+                                            urlToImage: widget.noticia.urlToImage),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
-                      ),
-                    ],
+                        SizedBox(height: 16),
+                        Text(
+                          "${widget.noticia.description ?? "Descripcion no disponible"}",
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          "${widget.noticia.author ?? "Autor no disponible"}",
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
-    ),
     );
+  }
+//https://github.com/himanshusharma89/sharefiles
+  Future<Null> _saveAndShare(New noticia) async {
+    var response = await get(Uri.parse(noticia.urlToImage));
+    final documentDirectory = (await getExternalStorageDirectory()).path;
+
+    File imgFile = new File('$documentDirectory/noticiaImagen.png');
+    imgFile.writeAsBytesSync(response.bodyBytes);
+
+    final RenderBox box = context.findRenderObject();
+    Share.shareFiles([('$documentDirectory/noticiaImagen.png')],
+        subject: 'Mira esta noticia',
+        text: '${noticia.title}: ${noticia.description}\n\n ${noticia.url}',
+        sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size);
   }
 }
